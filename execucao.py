c0d64322854e84f3abacb7fec92e896244809e53
limagustavokami <146367600+limagustavokami@users.noreply.github.com>
2025-07-03 20:30:39 -0300

    Update execucao.py

--- execucao.py
+++ execucao.py
@@ -12,11 +12,6 @@ async def run_combined_crawler():
     try:
         combined_urls = [
             "https://www.amazon.com.br/dp/B07Y1T5YZ2",
-            "https://www.mercadolivre.com.br/shampoo-wella-invigo-nutri-enrich-1-litro-profissional/p/MLB20570794/s",
-            "https://www.mercadolivre.com.br/shampoo-higienizando-widi-care-a-juba-500ml-limpeza-inteligente/p/MLB19860817/s?",
-            "https://www.mercadolivre.com.br/acidificante-widi-care-acidificando-a-juba-500ml/p/MLB36742897/s?",
-            "https://www.mercadolivre.com.br/wella-mascara-oil-reflections-500ml/p/MLB19512787/s?",
-            "https://www.mercadolivre.com.br/shampoo-higienizando-widi-care-a-juba-500ml-limpeza-inteligente/p/MLB19860817/s",
         ]
         
         await process_urls(combined_urls)
