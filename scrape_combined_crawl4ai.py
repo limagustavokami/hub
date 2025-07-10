9543527edb5a55d3f2fc1912b6edd0e89f8ceaaf
limagustavokami <146367600+limagustavokami@users.noreply.github.com>
2025-07-10 18:41:47 -0300

    Update scrape_combined_crawl4ai.py

--- scrape_combined_crawl4ai.py
+++ scrape_combined_crawl4ai.py
@@ -15,7 +15,7 @@ from playwright.async_api import async_playwright
 
 async def scrape_epoca_cosmeticos(url):
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=False)
+        browser = await playwright.chromium.launch(headless=True)
         context = await browser.new_context()
         page = await context.new_page()
         await page.goto(url)
