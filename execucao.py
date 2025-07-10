a28fb05c698fa56935a495b509798341cea27c34
limagustavokami <146367600+limagustavokami@users.noreply.github.com>
2025-07-10 11:05:53 -0300

    Update execucao.py

--- execucao.py
+++ execucao.py
@@ -11,6 +11,7 @@ async def run_combined_crawler():
     print(f'Executando scrape_combined_crawl4ai.py às {datetime.now()}')
     try:
         combined_urls = [
+            "https://www.mercadolivre.com.br/shampoo-invigo-color-brilliance-1l-wella-professionals/p/MLB19506213/s",
             "https://www.epocacosmeticos.com.br/pesquisa?q=8005610672427",
             'https://www.mercadolivre.com.br/shampoo-wella-invigo-nutri-enrich-1-litro-profissional/p/MLB20570794/s',
             "https://www.belezanaweb.com.br/wella-professionals-invigo-color-brilliance-shampoo-1-litro/ofertas-marketplace",
